9e333a7866a62a8a33a63d600b461d33c8f516eb
Sarathkumar-L <129084137+Sarathkumar-L@users.noreply.github.com>
2025-02-05 08:32:40 +0530

    Update User.java

--- 03-02-25/Java_Training/User.java
+++ 03-02-25/Java_Training/User.java
@@ -1,1 +1,53 @@
+package cust;
 
+public class User {
+
+    private String user_name;
+    private String user_email;
+    private long mob_num;
+    private String address;
+
+    public String getUser_name() {
+        return user_name;
+    }
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getUser_email() {
+        return user_email;
+    }
+    public void setUser_email(String user_email) {
+        this.user_email = user_email;
+    }
+
+    public long getMob_num() {
+        return mob_num;
+    }
+    public void setMob_num(long mob_num) {
+        this.mob_num = mob_num;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public User(String user_name, String user_email, long mob_num, String address){
+        this.user_name=user_name;
+        this.user_email=user_email;
+        this.mob_num=mob_num;
+        this.address=address;
+    }
+
+    public void displayUser(){
+        System.out.println("User: "+user_name);
+        System.out.println("Email:"+user_email);
+        System.out.println("Mobile:"+mob_num);
+        System.out.println("Address:"+address);
+    }
+
+
+}
