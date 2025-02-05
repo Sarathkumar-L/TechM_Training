31b705c80c09a3943f1c70d7ffd953204fd1dee5
Sarathkumar-L <129084137+Sarathkumar-L@users.noreply.github.com>
2025-02-05 08:31:55 +0530

    Update Main.java

--- 03-02-25/Java_Training/Main.java
+++ 03-02-25/Java_Training/Main.java
@@ -1,1 +1,9 @@
+package cust;
 
+public class Main {
+
+    public static void main(String[] args) {
+        User user=new User("Sk","skk@gmail.com",889977455, "No:39,main street, chennai");
+        user.displayUser();
+    }
+}
