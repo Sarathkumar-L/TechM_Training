5a4f19f0d1abd2ab09745d346aa588aad45130aa
Sarathkumar-L <129084137+Sarathkumar-L@users.noreply.github.com>
2025-02-17 09:37:43 +0530

    Create OddOrEven.js

---
+++ 17-02-2025 - Js/OddOrEven.js
@@ -0,0 +1,14 @@
+// Pass a number, and find if the sum of their digits is an even or odd number
+
+
+function multiplicationTable( num){
+    if(num % 2 == 0){
+        console.log("Even numberrr")
+    }
+    else{
+        console.log("Odd numberrr")
+    }
+    
+}
+
+multiplicationTable( 21)
