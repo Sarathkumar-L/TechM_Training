218168c37c43beeda023ab3dd9692ce517ebb1be
Sarathkumar-L <129084137+Sarathkumar-L@users.noreply.github.com>
2025-02-25 00:30:07 +0530

    Update Task1.java

--- Java consolidated tasks/Task -5/Task1.java
+++ Java consolidated tasks/Task -5/Task1.java
@@ -1,1 +1,12 @@
+public class Task1 {
+    public static void main(String[] args) {
+        new MyThread().start();
+    }
+}
 
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println("Greetings, Java!");
+    }
+}
