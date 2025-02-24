7c7465128c2dc32263afa60014e6600ec9d75771
Sarathkumar-L <129084137+Sarathkumar-L@users.noreply.github.com>
2025-02-25 00:35:48 +0530

    Update Task1.java

--- Java consolidated tasks/Task -7/Task1.java
+++ Java consolidated tasks/Task -7/Task1.java
@@ -1,1 +1,12 @@
+import java.util.ArrayList;
 
+public class Task1 {
+    public static void main(String[] args) {
+        ArrayList<String> colorPalette = new ArrayList<>();
+        colorPalette.add("Crimson");
+        colorPalette.add("Azure");
+        colorPalette.add("Emerald");
+        colorPalette.add("Gold");
+        System.out.println("Palette Collection: " + colorPalette);
+    }
+}
