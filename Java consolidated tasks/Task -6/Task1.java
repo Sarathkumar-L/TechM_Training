23c85016115259a8a4c6e205254ec8024176dcdf
Sarathkumar-L <129084137+Sarathkumar-L@users.noreply.github.com>
2025-02-25 00:34:12 +0530

    Update Task1.java

--- Java consolidated tasks/Task -6/Task1.java
+++ Java consolidated tasks/Task -6/Task1.java
@@ -1,1 +1,22 @@
 
+import java.util.List;
+
+public class Task1 {
+    public static <T extends Number> void calculateEvenOddSums(List<T> values) {
+        double sumOfEvens = 0, sumOfOdds = 0;
+        for (T value : values) {
+            if (value.intValue() % 2 == 0) {
+                sumOfEvens += value.doubleValue();
+            } else {
+                sumOfOdds += value.doubleValue();
+            }
+        }
+        System.out.println("Total of Even Numbers: " + sumOfEvens);
+        System.out.println("Total of Odd Numbers: " + sumOfOdds);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> sampleNumbers = List.of(3, 8, 5, 12, 7, 10, 15);
+        calculateEvenOddSums(sampleNumbers);
+    }
+}
