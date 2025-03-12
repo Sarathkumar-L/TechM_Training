bc8e1fee96a5d1246242f4ebb9221f8be79f592a
Sarathkumar-L <129084137+Sarathkumar-L@users.noreply.github.com>
2025-03-12 23:00:58 +0530

    Create info.ts

---
+++ Angular task/online learning platform/src/app/data/info.ts
@@ -0,0 +1,27 @@
+import { contact } from '../model/contacts';
+export const contacts:contact[] = [
+    {
+    name: "SRM University",
+    address: "No. 5/639, Old Mahabalipuram Road,",
+    city: "Chennai",
+    state: "Tamil Nadu",
+    phone: "044-2450-9530",
+    image: "srm.jpeg",
+    },
+    {
+    name: "Panimalar Institute of Tech",
+    address: "Unit No. 701, 7th Floor, B-Wing,",
+    city: "Poonamalle",
+    state: "Tamil nadu",
+    phone: "022-6645-1234",
+    image: "panimalar.png",
+    },
+    {
+        name:"Sairam engineering collage",
+        address:"Plot No. 32, Hitech City Main Road",
+        city:"Chennai",
+        state:"Tamil Nadu",
+        phone:"040-6789-3456",
+        image:"sairam.jpg",
+    }
+]
